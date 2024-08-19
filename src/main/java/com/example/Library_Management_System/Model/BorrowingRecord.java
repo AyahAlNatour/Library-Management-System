18dd9149df9448eb417e8b4b8a3ca4bdac88c727
AyahAlNatour <ayahnatour111@gmail.com>
2024-08-19 16:27:50 +0300

    Initial commit

---
+++ src/main/java/com/example/Library_Management_System/Model/BorrowingRecord.java
@@ -0,0 +1,19 @@
+@Entity
+public class BorrowingRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @ManyToOne
+    @JoinColumn(name = "patron_id")
+    private Patron patron;
+
+    private LocalDate borrowDate;
+    private LocalDate returnDate;
+
+    // Getters and Setters
+}
