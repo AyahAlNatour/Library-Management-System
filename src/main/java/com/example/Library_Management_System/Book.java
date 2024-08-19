5464c9a822407a68dbdabe0055b850373e5572b2
AyahAlNatour <137375810+AyahAlNatour@users.noreply.github.com>
2024-08-19 15:58:52 +0300

    Create Book.java

---
+++ src/main/java/com/example/Library_Management_System/Book.java
@@ -0,0 +1,12 @@
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private int publicationYear;
+    private String isbn;
+
+    // Getters and Setters
+}
